6bb4c75a591c9637925040cb2756cf1f81580395
Crowdbotics <team@crowdbotics.com>
2023-04-07 08:20:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wo-ai-furniture-39774",
+  url: "https://wo-ai-furniture-39774.botics.co",
+  api: "https://wo-ai-furniture-39774.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
