49982b2efe8b676822982d4bd3dfd81ea9a22bcb
hanwen li <hanwenli2000@gmail.com>
2023-04-07 04:54:24 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "wo-ai-furniture-39774",
-  url: "https://wo-ai-furniture-39774.botics.co",
-  api: "https://wo-ai-furniture-39774.botics.co/api/v1"
-}
+  "name": "wo-ai-furniture-39774",
+  "url": "https://wo-ai-furniture-39774.botics.co",
+  "api": "https://wo-ai-furniture-39774.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
